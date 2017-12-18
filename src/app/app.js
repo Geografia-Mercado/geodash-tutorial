b63003df988859478f5c5e38984dee1548e1e335
yano mateus <yano.mateus@gmail.com>
2017-12-18 02:12:30 -0200

    angular material implementado

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,11 @@
 import angular from 'angular';
 import uiRouter from '../../node_modules/@uirouter/angularjs/release/angular-ui-router.js'
 
+import '../../node_modules/angular-material/angular-material.css'
+import '../../node_modules/angular-material/angular-material.js'
+import '../../node_modules/angular-animate/angular-animate.js'
+import '../../node_modules/angular-aria/angular-aria.js'
+
 import Config from './config.js'
 
 import AppDirective from './app.directive.js'
@@ -9,7 +14,7 @@ import LoginController from './layout/login/login.controller.js'
 
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, [uiRouter])
+angular.module(MODULE_NAME, [uiRouter, 'ngAria', 'ngAnimate', 'ngMaterial'])
 .config(['$stateProvider', Config])
 .directive('app', AppDirective)
 .controller('loginController', LoginController)
